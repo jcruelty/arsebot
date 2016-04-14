9435f83c1d280a709eb2c1b4b0b66198f1a5a9a9
Amar Pai <jcruelty1@yahoo.com>
2016-04-14 20:28:38 +0000

    Add tacos functionality

--- arsebot.rb
+++ arsebot.rb
@@ -84,6 +84,9 @@ class ArseBot < SlackRubyBot::Bot
   #
   #   client.say(text: "#{sequence} (#{sequence.size})")
   # end
+  match /tacos/ do |client, data, match|
+    client.say(text: "DID SOMEONE SAY TACOS?!?!?! mmm mmm", channel: data.channel)
+  end
 
   match /^lcs (?<user1>\w*) (?<user2>\w*)$/ do |client, data, match|
     # today lookup usernames of cur user, all other users, use those
